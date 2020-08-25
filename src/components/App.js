2f27c6bb54df7b7102fbea369b07a8b3d3279764
kgov <kim.gov@latitudefinancial.com>
2020-08-25 20:38:05 +1000

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import emojipedia from '../emojipedia';
+import Entry from './Entry';
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      meaning={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className='dictionary'>
+        {emojipedia.map(createEntry)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,109 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: '💪',
+    name: 'Tense Biceps',
+    meaning:
+      '“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.'
+  },
+  {
+    id: 2,
+    emoji: '🙏',
+    name: 'Person With Folded Hands',
+    meaning:
+      'Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.'
+  },
+  {
+    id: 3,
+    emoji: '🤣',
+    name: 'Rolling On The Floor, Laughing',
+    meaning:
+      'This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.'
+  },
+  {
+    id: 4,
+    emoji: '😄',
+    name: 'Smiling Face With Open Mouth And Smiling Eyes',
+    meaning:
+      'Smiley\'s mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm.'
+  },
+  {
+    id: 5,
+    emoji: '😅',
+    name: 'Smiling Face With Open Mouth & Cold Sweat',
+    meaning:
+      'Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous.'
+  },
+  {
+    id: 6,
+    emoji: '💩',
+    name: 'Pile of Poo',
+    meaning:
+      'Crap, oh no! Such a sh**! The comic version of a pile of feces. Can describe a situation, replace the swear word, or criticize a statement of the chat partner.'
+  },
+  {
+    id: 7,
+    emoji: '🙃',
+    name: 'Upside-Down Face',
+    meaning:
+      'Don\'t take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke.'
+  },
+  {
+    id: 8,
+    emoji: '😎',
+    name: 'Face With Sunglasses',
+    meaning:
+      'I\'m the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach.'
+  },
+  {
+    id: 9,
+    emoji: '😖',
+    name: 'Confounded Face',
+    meaning:
+      'How on earth could that happen? Smiley is extremely dismayed and bewildered. Or damn angry: That\'s almost the final straw!'
+  },
+  {
+    id: 10,
+    emoji: '🤔',
+    name: 'Thinking Face',
+    meaning:
+      'The thinker\'s pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person\'s intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea.'
+  },
+  {
+    id: 11,
+    emoji: '😶 ',
+    name: 'Face Without Mouth',
+    meaning:
+      'Emoji without a mouth. Is speechless, has no words. Someone does not want to or cannot say anything about a specific topic. Used in difficult, embarrassing or bad conversations. Imagine what it would say if it had a mouth!'
+  },
+  {
+    id: 12,
+    emoji: '👩‍💻',
+    name: 'Female technologist',
+    meaning:
+      'You spend too much time online! A computer freak, student, blogger or online shopping enthusiast. The woman spends a lot of time in front of the computer because of her hobby, her education or her job.'
+  },
+  {
+    id: 13,
+    emoji: '🖕',
+    name: 'Reversed Hand With Middle Finger Extended',
+    meaning:
+      'In western culture, the middle finger smiley is used as a rude and offensive gesture.'
+  },
+  {
+    id: 14,
+    emoji: '👋',
+    name: 'Waving Hand',
+    meaning:
+      'Hello and goodbye! Have a good trip! A friendly waving hand. Can be used as a welcome or farewell. Or sarcastically, if you wished someone or something would disappear.'
+  },
+  {
+    id: 15,
+    emoji: '👍',
+    name: '“Thumbs-Up” Sign',
+    meaning:
+      'Well done! Hand with thumb turned up. Stands for commitment, agreement and approval! Caution: In Arab countries, this is interpreted as showing the two fingers.'
+  }
+];
+
+export default emojipedia;
